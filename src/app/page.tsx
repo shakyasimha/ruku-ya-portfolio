d1e18d0d8ea4d41fa243b1c0fb62feb6d3fc6b83
Sushovan Shakya <sushakya235@proton.me>
2025-05-30 07:11:27 +0545

    still working on making the profile more responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       </div>
 
       {/* Profile header section */}
-      <div className="flex flex-col md:flex-row">
+      <div className="flex flex-col md:flex-row items-center mt-[-4rem] px-4">
         {/* Profile picture */}
-        <div className="relative w-96 h-96 rounded-full overflow-hidden border-1 border-black p-1">
+        <div className="relative w-40 h-40 rounded-full overflow-hidden border-4 border-white mx-auto md:mx-0 z-10">
           <div className="relative w-full h-full rounded-full overflow-hidden">
             <Image
               src="/rukshana-1-copy.jpg"
@@ -25,10 +25,14 @@ export default function Home() {
               className="object-cover"
             />
           </div>
-
-          
         </div>
-      </div>  
-    </div>
+
+        {/* Client name, client description and contact buttons */}
+        <div className="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+          <h1 className="text-2xl font-bold">Rukshana Kapali</h1>
+          <p className="text-[#333]">Lawyer, Activist, Linguist.</p>
+        </div>
+      </div>
+    </div> 
   );
 }
