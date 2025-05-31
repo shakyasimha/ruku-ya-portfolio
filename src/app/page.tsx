3d64b42d4e243729abba3b12aa5d5105f24ea648
Sushovan Shakya <sushakya235@proton.me>
2025-05-31 23:12:11 +0545

    added components for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,21 +15,19 @@ export default function Home() {
       </div>
 
       {/* Profile header section */}
-      <div className="flex flex-col md:flex-row items-center mt-[-6rem] px-4">
+      <div className="flex flex-col md:flex-row items-center mt-[-6rem] px-4 w-full max-w-4xl mx-auto">
         {/* Profile picture */}
         <div className="relative w-60 h-60 rounded-full overflow-hidden border-4 border-white mx-auto md:mx-0 z-10">
-          <div className="relative w-full h-full rounded-full overflow-hidden">
-            <Image
-              src="/rukshana-1-copy.jpg"
-              alt="rukshana profile picture"
-              fill
-              className="object-cover"
-            />
-          </div>
+          <Image
+            src="/rukshana-1-copy.jpg"
+            alt="rukshana profile picture"
+            fill
+            className="object-cover"
+          />
         </div>
 
         {/* Client name, client description and contact buttons */}
-        <div className="flex flex-col md:flex-row items-center justify-between w-full mt-4 md:mt-8 max-w-4xl">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full mt-4 md:mt-8">
           <div className="text-center md:text-left md:ml-8 md:mt-4">
             <h1 className="text-3xl font-bold">Rukshana Kapali</h1>
             <p className="text-[#333] text-xl">Lawyer, Activist, Linguist.</p>
@@ -41,8 +39,6 @@ export default function Home() {
             <button className="px-4 py-2 border border-gray-300 rounded">Message</button>
           </div>
         </div>
-
-        
       </div>
 
       {/* Horizontal divider section */}
