ca585738f77bfdbacfac77fb1ff105cd58bd16a2
Sushovan Shakya <sushakya235@proton.me>
2025-06-02 16:30:29 +0545

    going to add button for changing the language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,38 +3,33 @@ import Link from "next/link";
 
 import Header from "@/components/Header";
 
-const defaultInfo = {
-  name: "Rukshana Kapali",
-  subtitle: "Lawyer, Linguist, Activist.",
-  call: "Call",
-  message: "Message"
-};
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header 
-        headerInfo={defaultInfo}
-      />
+      <Header title="Rukshana Kapali"/>
 
       {/* Horizontal divider section */}
-      <hr className="border-t border-gray-600 mt-6 w-full max-w-4xl" />
+      <hr className="mx-auto w-1/2 border-t border-gray-300 mt-8" />
 
       {/* Content section */}
       <div className="flex flex-col md:flex-row gap-6 p-4 max-w-4xl mx-auto mt-8">
         {/* About Me section */}
-        <div className="flex-1">
+        <div className="flex-1 md:mr-4 mx-4">
           <h2 className="text-xl font-semibold">About Me</h2>
-          <p className="text-gray-700 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <p className="text-gray-700 mt-2 text-justify">
+            My interest and work involves Language, Linguistics, Newa issues, Nepala Bhasa, Law, and SOGIESC issues. I engage in knowledge production through research, awareness toolkit publications, academic writing, and so on. I also engage in policy and legal issues, in which I mostly center on litigation. <br />
           </p>
-          <p className="text-gray-700 mt-2">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...
+          <p className="text-gray-700 mt-2 text-justify">
+            Since N.S. 1141 (2020), my work has become more focused on SOGIESC issues, through registration of Queer Youth Group (QYG) as a not-for-profit, which was a loose network I co-initiated. As a part of QYG, my work has mostly entailed engaging in production of resource materials on SOGIESC. Similarly, I have focused on legal activism through litigation, which entails filing writ petitions at Supreme Court & High Courts on issues of concern to the PoMSOGIESC demography, providing legal assistance to PoMSOGIESC individuals on issues pertaining to their rights, which has predominantly been about, but not limited to, legal gender recognition issues of transgender folks.
+          </p>
+          <p className="text-gray-700 mt-2 text-justify">
+            Since N.S. 1143 (2022), I began working around Jugi community’s traditional abode of Satachhen. I have been lobbying, campaigning and petitioning to ensure right of ownership, right to housing and several other aspects of the community’s historical residence.
           </p>
         </div>
 
         {/* Location, contact details */}
-        <div className="flex-1 space-y-4">
+        <div className="flex-1 space-y-4 mx-4 md:ml-16 mt-2">
           <div>
             <h2 className="text-xl font-bold">Location</h2>
             <div className="flex items-center gap-2 mt-1">
@@ -50,12 +45,12 @@ export default function Home() {
 
           <div>
             <h2 className="text-xl font-bold">Email</h2>
-            <p>rukshana@example.com</p>
+            <p>rukshanakapali1144@gmail.com</p>
           </div>
 
           <div>
             <h2 className="text-xl font-bold">Contact Number</h2>
-            <p>+977-98XXXXXXX</p>
+            <p>+977-9808262699</p>
           </div>
         </div>
       </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
+import { Alegreya_Sans, Alegreya_Sans_SC } from "next/font/google";
+
+// Fonts 
+const alegreyaSans = Alegreya_Sans({
+    weight: ['400', '500', '700'],
+    subsets: ["latin"],
+});
+
+const alegreyaSC = Alegreya_Sans_SC({ 
+    weight: ['500', '700'],
+    subsets: ["latin"],
+});
 
 interface HeaderProps {
-    headerInfo: {
-        name: string, 
-        subtitle: string, 
-        call: string, 
-        message: string
-    }
+    title: string
 };
 
-export default function Page({ headerInfo }: HeaderProps) {
+export default function Page({ title }: HeaderProps) {
     return(
         <div className="flex flex-col">
             {/* Cover photo */}
@@ -27,24 +36,23 @@ export default function Page({ headerInfo }: HeaderProps) {
                 {/* Profile picture */}
                 <div className="relative flex-shrink-0 w-60 h-60 rounded-full overflow-hidden border-4 border-white mx-auto md:mx-0 z-10">
                     <Image
-                    src="/rukshana-1-copy.jpg"
-                    alt="rukshana profile picture"
-                    fill
-                    className="object-cover"
+                        src="/rukshana-1-copy.jpg"
+                        alt="rukshana profile picture"
+                        fill
+                        className="object-cover"
                     />
                 </div>
         
                 {/* Client name, client description and contact buttons */}
-                <div className="flex flex-col md:flex-row items-center justify-between w-full mt-4 md:mt-8">
+                <div className="flex flex-col items-center justify-between w-full mt-4 md:mt-8">
                     <div className="text-center md:text-left md:ml-8 md:mt-4">
-                        <h1 className="text-3xl font-bold">{headerInfo.name}</h1>
-                        <p className="text-[#333] text-xl">{headerInfo.subtitle}</p>
+                        <h1 className={`${alegreyaSC.className} text-3xl font-bold`}>{title}</h1>
+                        {/* <p className={`${alegreyaSans.className} text-xl text-[#333]`}>{headerInfo.subtitle}</p> */}
                     </div>
-
-                    {/* Buttons */}
-                    <div className="flex gap-2 mt-4 md:mt-0 justify-center md:justify-end">
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded">{headerInfo.call}</button>
-                        <button className="px-4 py-2 border border-gray-300 rounded">{headerInfo.message}</button>
+                    
+                    {/* This part contains links for the website in different languages */}
+                    <div className="text-center flex-row">
+                        
                     </div>
                 </div>
             </div>
